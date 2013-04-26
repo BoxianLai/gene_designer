5919f7d99375ba00165b5e7b4fc05506eead30b7
luxuia <luxujia@genomics.com>
2013-04-26 13:36:35 +0800

    add feature in BioMangager

--- BioManager.py
+++ BioManager.py
@@ -1,24 +1,50 @@
 #!/usr/bin/env python
 
 from Bio import SeqIO
+
 class BioManager:
+    seq = ""
+    record = ""
+    mRNA = []
+
+    def __init__(self, filename='t7.gb', filetype='genbank'):
+        self.record = SeqIO.parse(filename, filetype).next()
+        self.seq = self.record.seq
+
     def get_first_CDS(self, parser):
         for feat in parser.features:
             if feat.type == 'CDS':
                 yield feat
 
-    def parse(self, filename='sequence.gb', filetype='genbank'):
-        record = SeqIO.parse(filename, filetype).next()
-        for a in self.get_first_CDS(record):
-            if ('gene' in a.qualifiers):
-                yield dict(name=a.qualifiers['gene'][0], \
-                            start=a.location.start, \
-                            end = a.location.end)
+    def parse(self, filename='t7.gb', filetype='genbank'):
+        for feat in self.record.features:
+            if feat.type == 'mRNA':
+                mrna = [{}]
+                mrna[0]['start'] = int(feat.location.start)
+                mrna[0]['end']   = int(feat.location.end)
+                mrna[0]['tag']   = 'mRNA'
+                self.mRNA.append(mrna)
+            elif feat.type == 'CDS':
+                cds = {}
+                cds['start'] = int(feat.location.start)
+                cds['end']   = int(feat.location.end)
+                cds['tag'] = 'CDS'
+                cds['translation'] = feat.qualifiers['translation']
+                self.mRNA[len(self.mRNA)-1].append(cds)
+        return self.mRNA
 
 
 if __name__ == '__main__':
     app = BioManager()
     result = app.parse()
-    print ( [ a for a in result] )
+    for rna in result:
+        for seq in rna:
+            if seq['tag'] == 'mRNA':
+                print "{0}({1}-{2})".format(seq['tag'],\
+                                seq['start'], seq['end'])
+            if seq['tag'] == 'CDS':
+                print "-->{0}({1}-{2})".format(seq['tag'],\
+                                seq['start'], seq['end'])
+        raw_input("")
 
 
--- dry.py
+++ dry.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 
+from __future__ import division
 from flask import Flask
 from BioManager import *
 
 app = Flask(__name__)
 bio = BioManager()
+randColor = ['#0099ff', '#00ff99',
+            '#ff0099', '#ff9900',
+            '#99ff00', '#9900ff']
 
 @app.route('/<message>')
 def output(message):
     result = bio.parse()
-    html = '<table border="1"> <tr>'
+    totalLen = len(bio.seq)
+    html = '<table border="1" cellspacing="0"> <tr>'
     result = [a for a in result]
-    for index in xrange(1, len(result)-1):
-        html += "<td>{0}</td>".format(\
-                result[index]['name'])
+    resultNum = len(result)
+    for index in xrange(1, resultNum-1):
+        html += '<td width="{1}px" bgColor={2}>{0}</td>'.format(\
+                result[index]['name'], \
+                (result[index]['end']-result[index]['start'])\
+                /( totalLen/resultNum ) * (1/0.06188888888),\
+                randColor[index%6])
     html += '</table>'
     return html
 
